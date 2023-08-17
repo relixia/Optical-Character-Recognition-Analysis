7e65016db72221e0c351f913312342881a9b190a
Buğra Çayır <bugra-net@hotmail.com>
2023-08-17 17:02:54 +0300

    index page and image upload done

--- ocr_analysis/main.py
+++ ocr_analysis/main.py
@@ -1,10 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
+
 
 app = FastAPI()
 
-@app.get('/search')
-def search():
-    return {"message": "Hello World"}
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="templates/static"), name="static")
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post('/result', response_class=HTMLResponse)
+async def upload_file(
+    request: Request, upload: UploadFile = File(...)):
+    print("Inside /result route")
+    print("Uploaded filename:", upload.filename)
+
+    return templates.TemplateResponse("result.html", {"uploaded_file_name": upload.filename, "request": request,})
+
 
 if __name__ == '__main__':
     import uvicorn
